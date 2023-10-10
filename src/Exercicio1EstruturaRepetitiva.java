8f9cae243c1dd84fa410fd9b3c556b97dc409e84
vinicinrn <zviniciusnascimento@gmail.com>
2023-10-10 17:02:33 -0300

    Revisando Estrutura Repetitiva (while)

---
+++ src/Exercicio1EstruturaRepetitiva.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exercicio1EstruturaRepetitiva {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Para fins de teste, insira uma senha diferente de 2002:");
+        int senha = sc.nextInt();
+
+        while(senha!=2002){
+            System.out.println("Senha invalida");
+            System.out.println("Insira outra senha");
+            senha = sc.nextInt();
+        }
+        System.out.println("Acesso Permitido");
+    }
+}
