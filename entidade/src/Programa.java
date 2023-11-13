6175984dc75e3553bdbf602bf602aad84b297ad3
vinicinrn <zviniciusnascimento@gmail.com>
2023-11-13 06:46:30 -0300

    Revisando conceitos iniciais em Orientação a Objetos

--- entidade/src/Programa.java
+++ entidade/src/Programa.java
@@ -24,11 +24,8 @@ public class Programa {
         y.b = sc.nextDouble();
         y.c = sc.nextDouble();
 
-        double p = (x.a + x.b + x.c)/2;
-        double areaX = Math.sqrt(p*(p-x.a)*(p-x.b)*(p-x.c));
-
-        p = (y.a + y.b + y.c)/2;
-        double areaY = Math.sqrt(p*(p-y.a)*(p-y.b)*(p-y.c));
+        double areaX = x.area();
+        double areaY = y.area();
 
         System.out.printf("Area do triângulo A: %.2f%n", areaX);
         System.out.printf("Area do triângulo B: %.2f%n", areaY);
